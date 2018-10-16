4a6deae01d9f6b6ceba1a479f14c27429d6d9c4b
张江源 <1225999757@qq.com>
2018-10-16 11:19:04 +0800

    提交

---
+++ .idea/test/src/test.java
@@ -0,0 +1,12 @@
+public class test {
+    public static void main(String[] args){
+        System.out.println("test----");
+        oricle(2,5);
+        System.out.println("test2----");
+
+    }
+    public static int oricle(int width;int height){
+        return width*height;
+    }
+
+}
